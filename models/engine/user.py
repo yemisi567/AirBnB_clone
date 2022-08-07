44f7d41a81da975e85e592b895e170773be5f3ca
yemisi567 <51059680+yemisi567@users.noreply.github.com>
2022-08-07 21:41:53 +0100

    airbnb

---
+++ models/engine/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is User class to represent new users
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ User subclass that inherits from BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
